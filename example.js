3fa7be6b72d98f3b8f8a7d045d82ea5b3006092b
Peter Evers <pevers90@gmail.com>
2015-12-04 12:45:18 +0100

    added yahoo scraper

--- example.js
+++ example.js
@@ -3,41 +3,48 @@
 var Scraper = require ('./index')
   , google = new Scraper.Google()
   , bing = new Scraper.Bing()
-  , pics = new Scraper.Picsearch();
+  , pics = new Scraper.Picsearch()
+  , yahoo = new Scraper.Yahoo();
 
 // will take ALOT of time if num=undefined
-// google.list({
-// 	keyword: 'coca cola',
-// 	num: 10,
-// 	nightmare: {
-// 		show: true
-// 	}
-// 	//resolution:'l',
-// })
-// .then(function (res) {
-// 	console.log('first 10 results from google', res);
-// }).catch(function(err) {
-// 	console.log('err',err);
-// });
+google.list({
+	keyword: 'coca cola',
+	num: 10,
+	nightmare: {
+		show: true
+	}
+	//resolution:'l',
+})
+.then(function (res) {
+	console.log('first 10 results from google', res);
+}).catch(function(err) {
+	console.log('err',err);
+});
 
-// bing.list({
-// 	keyword: 'banana',
-// 	num: 10
-// })
-// .then(function (res) {
-// 	console.log('first 10 results from bing', res);
-// }).catch(function(err) {
-// 	console.log('err',err);
-// })
+bing.list({
+	keyword: 'banana',
+	num: 10
+})
+.then(function (res) {
+	console.log('first 10 results from bing', res);
+}).catch(function(err) {
+	console.log('err',err);
+})
 
 pics.list({
 	keyword: 'banana',
 	num: 10,
-	detail: true
 }).then(function (res) {
 	console.log('out',res);
 }).catch(function (err) {
 	console.log('err',err);
 });
 
-// yahoo.list();
+yahoo.list({
+	keyword: 'banana',
+	num: 10,
+}).then(function (res) {
+	console.log('results',res);
+}).catch(function (err) {
+	console.log('err',err);
+});
