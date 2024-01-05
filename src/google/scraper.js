2c725da235cb9a957ea0d17b5baf1115c466ca3b
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2024-01-06 01:11:10 +0300

    Support more selectors

--- src/google/scraper.js
+++ src/google/scraper.js
@@ -197,7 +197,10 @@ class GoogleScraper {
 
     $('#islrg div[jsaction][data-tbnid]').each(function (_i, containerElement) {
       const containerElement_ = $(containerElement);
-      const linkElementHref = containerElement_.find("a[href^='/imgres']").attr('href');
+      const linkElementHrefExpectedSelectors = ["a[href^='/imgres']", "a[jsaction]"];
+      const linkElementHref = linkElementHrefExpectedSelectors
+        .map(s => containerElement_.find(s).attr('href'))
+        .filter(e => e);
       if (linkElementHref) { // linkElementHref could be undefined
         const imageElementAlt = containerElement_.find('img').attr('alt');
         const parsedLink = url.parse(linkElementHref, { parseQueryString: true });
